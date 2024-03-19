d1bd582e94a25925954cd192b3504ec34837af7b
akrathore212 <163972573+akrathore212@users.noreply.github.com>
2024-03-19 11:14:53 +0530

    Add files via upload

---
+++ Portfolio/main.js
@@ -0,0 +1,16 @@
+const menuBtn =document.querySelector('#open-menu-btn');
+const closeBtn =document.querySelector('#close-menu-btn');
+const menu=document.querySelector('.nav_menu');
+
+menuBtn.addEventListener('click',()=>{
+    menu.style.display='block';
+    closeBtn.style.display='inline-block';
+    menuBtn.style.display='none';
+})
+
+const closeNav =()=>{
+    menu.style.display='none';
+    closeBtn.style.display='none';
+    menuBtn.style.display='inline-block';
+}
+closeBtn.addEventListener('click',closeNav);
